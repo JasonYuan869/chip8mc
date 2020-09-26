abdbd7b793261a9f266e9d86a2e5b8672fde8a88
Jason Yuan <jason.yuan869@gmail.com>
2020-09-26 16:15:49 -0400

    try a new testrom

--- python_helpers/rom_to_scoreboard.py
+++ python_helpers/rom_to_scoreboard.py
@@ -1,10 +1,10 @@
 # Load a ch8 file (binary) into scoreboard instructions
 
-with open('test_opcode.ch8','rb') as f:
+with open('c8_test.c8','rb') as f:
     byte_array = bytes(f.read())
 
-with open('..\data\\roms\\test_rom.mcfunction', 'w') as f:
+with open('..\data\\roms\\functions\\test_rom.mcfunction', 'w') as f:
     i = 0
     for byte in byte_array:
-        f.write(f'scoreboard players set Global mem_{i + 200} {str(byte)}\n')
+        f.write(f'scoreboard players set Global mem_{i + 512} {str(byte)}\n')
         i += 1
