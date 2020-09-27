cf4250f931ebca2ff7603ccb2ee4035fa70da776
Jason Yuan <jason.yuan869@gmail.com>
2020-09-27 15:14:04 -0400

    add more roms, remove test roms

--- data/roms/functions/8xye.mcfunction
+++
@@ -1,4 +0,0 @@
-scoreboard players set Global mem_512 96
-scoreboard players set Global mem_513 116
-scoreboard players set Global mem_514 128
-scoreboard players set Global mem_515 14
--- python_helpers/rom_to_scoreboard.py
+++ python_helpers/rom_to_scoreboard.py
@@ -1,9 +1,9 @@
 # Load a ch8 file (binary) into scoreboard instructions
 
-with open('f01e.ch8','rb') as f:
+with open('Breakout [Carmelo Cortez, 1979].ch8','rb') as f:
     byte_array = bytes(f.read())
 
-with open('..\data\\roms\\functions\\f01e.mcfunction', 'w') as f:
+with open('..\data\\roms\\functions\\breakout.mcfunction', 'w') as f:
     i = 0
     for byte in byte_array:
         f.write(f'scoreboard players set Global mem_{i + 512} {str(byte)}\n')
